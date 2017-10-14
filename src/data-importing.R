3a93a83fddd96fc4c100d3828089c3bb5a59e3e8
minutestatistique <data.science.fr@gmail.com>
2017-10-14 01:32:52 +0000

    continued data definitions

--- src/data-importing.R
+++ src/data-importing.R
@@ -12,14 +12,24 @@ data_def <- fromJSON("conf/data-def.json")
 # data importing
 #-------------------------------------------------------------------------------
 ### gdelt
-gdelt <- fread("data/qualif_bdd_marques_v2.csv",
-             header = TRUE, sep = ";",
-             colClasses = rep("character",
-                              length(MyVarNameXtract(data_def, "gdelt"))))
+gdelt <- fread("data/gdelt.tsv",
+               colClasses = rep("character",
+                                length(MyVarNameXtract(data_def, "gdelt"))))
+setnames(gdelt, names(gdelt), MyVarNameXtract(data_def, "gdelt"))
 testthat::expect_identical(names(gdelt), MyVarNameXtract(data_def, "gdelt"))
-MyDataTypeConvert(data_def, "gdelt", dec = ",", verbose = TRUE)
+MyDataTypeConvert(data_def, "gdelt", dec = ".", verbose = TRUE)
 save(gdelt, file = "data/RData/gdelt.RData")
 rm(gdelt)
+gc()
+
+### gkg
+gkg <- fread("data/gkg.tsv",
+             colClasses = rep("character",
+                              length(MyVarNameXtract(data_def, "gkg"))))
+testthat::expect_identical(names(gkg), MyVarNameXtract(data_def, "gkg"))
+MyDataTypeConvert(data_def, "gkg", dec = ".", verbose = TRUE)
+save(gkg, file = "data/RData/gkg.RData")
+rm(gkg)
 gc()
 
 rm(data_def)
---
+++ src/main.R
@@ -0,0 +1,3 @@
+source("utl/packages-installer.R")
+source("utl/utl.R")
+source("src/data-importing.R")
